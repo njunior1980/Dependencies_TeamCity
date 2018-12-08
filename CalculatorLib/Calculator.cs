156cc1ff548ebcf5f9d50ae9ebce02d6d8579cee
Nelson Souza <njunior1980@gmail.com>
2018-12-08 14:07:29 +0000

    Update Calculator.cs

--- CalculatorLib/Calculator.cs
+++ CalculatorLib/Calculator.cs
@@ -2,7 +2,7 @@
 
 namespace CalculatorLib
 {
-    public class Calculator
+    public class Calculator2
     {
         public int Sum(int n1, int n2) => n1 + n2;
         public int Subtract(int n1, int n2) => n1 - n2;
